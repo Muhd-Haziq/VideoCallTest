4e7d86f81f525499f5d41944d654e0970c1785ce
19047241 <19047241@rp.edu,sg>
2021-05-04 23:29:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/s19047241/videocalltest/MainActivity.java
@@ -0,0 +1,49 @@
+package sg.edu.rp.c346.s19047241.videocalltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etRoom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etRoom = findViewById(R.id.conferenceName);
+
+        try {
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(new URL(""))
+
+                    .setWelcomePageEnabled(false)
+                    .build();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onButtonClick(View view){
+        String text = etRoom.getText().toString();
+
+        if(text.length() > 0){
+            JitsiMeetConferenceOptions options
+                    = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .build();
+            JitsiMeetActivity.launch(this,options);
+        }
+    }
+}
